e771a1f80306c8345f9a4a220aa5a6d37d1908f3
Sofiya <sofiabelskaya@me.com>
2020-11-16 23:55:02 +0300

    Update main.cpp

--- 06/main.cpp
+++ 06/main.cpp
@@ -12,30 +12,30 @@ int main()
     auto text = format("{1}+{1} = {0}", 2, "one");
     assert(text == "one+one = 2");
     try {
-        auto text = format("{2}+{1} = {0}", 2, "one");
+        text = format("{2}+{1} = {0}", 2, "one");
     }
-    catch (const FormatException &ex) {
+    catch (const FormatException& ex) {
         if (ex.get_error() == "invalid argument for {}: index out of range ")
             cout << "test1 success" << endl;
     }
     try {
-        auto text = format("2}+{1} = {0}", 2, "one");
+        text = format("2}+{1} = {0}", 2, "one");
     }
-    catch (const FormatException &ex) {
+    catch (const FormatException& ex) {
         if (ex.get_error() == "expected '{' before '}' ")
             cout << "test2 success" << endl;
     }
     try {
-        auto text = format("{2", 2, "one");
+        text = format("{2", 2, "one");
     }
-    catch (const FormatException &ex) {
+    catch (const FormatException& ex) {
         if (ex.get_error() == "expected '}' after '{' ")
             cout << "test3 success" << endl;
     }
     try {
-        auto text = format("{gf}+{1} = {0}", 2, "one");
+        text = format("{gf}+{1} = {0}", 2, "one");
     }
-    catch (const FormatException &ex) {
+    catch (const FormatException& ex) {
         if (ex.get_error() == "invalid argument for {} ")
             cout << "test4 success" << endl;
     }
